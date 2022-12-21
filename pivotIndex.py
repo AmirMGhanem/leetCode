06fbe0f905781df422eb9ca9c2beb0c16929e6b9
Amir m. Ghanem <55459991+AmirMGhanem@users.noreply.github.com>
2022-12-21 21:40:37 +0200

    PivotIndex

---
+++ pivotIndex.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 1 :
+            return 0
+        for x in range(len(nums)):
+            left = nums[0:x]
+            right = nums[x+1:]
+            sum_left=sum(left)
+            sum_right=sum(right)
+            if sum_right==sum_left:
+                return x
+        return -1
+
+s = Solution()
+print(s.pivotIndex([-1,-1,-1,-1,-1,0]))
