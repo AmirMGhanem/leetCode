1bc8b27a1f6ee7572121cba9dfede484cc72ae0b
Amir m. Ghanem <55459991+AmirMGhanem@users.noreply.github.com>
2022-12-27 12:45:21 +0200

    longestPalindrome

--- longestPalindrome.py
+++ longestPalindrome.py
@@ -1,7 +1,27 @@
+from collections import Counter
 class Solution(object):
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: int
         """
+        _sum=0
+        counter=Counter(s)
+        odd=False
+        print(counter)
+        for c in counter.values():
+            _sum+=c /2 *2
+            if _sum%2==0 and c % 2 !=0:
+                _sum+=1
+        return _sum
 
+
+
+
+
+
+
+
+
+s= Solution()
+print(s.longestPalindrome("ccc"))
