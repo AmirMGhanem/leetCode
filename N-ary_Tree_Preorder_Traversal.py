627b97b805be4091e355cf8d60a586e48d906154
Amir m. Ghanem <55459991+AmirMGhanem@users.noreply.github.com>
2022-12-27 14:36:56 +0200

    N-ary_Tree_Preorder_Traversal

---
+++ N-ary_Tree_Preorder_Traversal.py
@@ -0,0 +1,30 @@
+
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        result=[]
+        if not root : return []
+        s = [root]
+        while len(s)>0:
+            r = s.pop()
+            result.append(r.val)
+            s.extend(reversed(r.children))
+        return result
+
+
+
+
+
+
+
+
